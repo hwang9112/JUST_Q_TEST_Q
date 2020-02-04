3bc7f3e7af873ebccf361a5aa23eab35e6b3ec15
hwang9112 <43162006+hwang9112@users.noreply.github.com>
2020-02-04 21:33:25 +0900

    데이터베이스 내용입력, 출력
    1. 데이터베이스 내 데이터 입력
    2. 데이터베이스 내 데이터 출력(확인)
    추가완료

--- Xslx_to_DB.py
+++ Xslx_to_DB.py
@@ -12,12 +12,38 @@ if table_check == -1:
                   "(OrderDate date, Region text, Rep text, Item text, Units num, Unit_Cost float, Total float);"
     Database.excute_Query(cursor, table_query)
     DBconn.commit() #커밋 필수
-table_check = Database.excute_Query(cursor,"select * from SampleData") #테이블 생성 체크 쿼리
+
 #Excel 불러와서 변수에 저장
 excel_data = pd.read_excel("Sample_Data\SampleData.xlsx",sheet_name="SalesOrders")
-print(excel_data)
+
 #Excel 데이터 입력 쿼리
+for split_data in range(len(excel_data)):
+    # print(excel_data.loc[split_data])
+    Q_data1 = excel_data.loc[split_data]['OrderDate']
+    Q_data1 = pd.to_datetime(Q_data1) #Date타입 변환용
+    Q_data2 = excel_data.loc[split_data]['Region']
+    Q_data3 = excel_data.loc[split_data]['Rep']
+    Q_data4 = excel_data.loc[split_data]['Item']
+    Q_data5 = excel_data.loc[split_data]['Units']
+    Q_data6 = excel_data.loc[split_data]['Unit Cost']
+    Q_data7 = excel_data.loc[split_data]['Total']
+    insert_query = "INSERT INTO SampleData VALUES(" \
+                   "'" + str(Q_data1) + "',"  \
+                   "'" + str(Q_data2) + "',"  \
+                   "'" + str(Q_data3) + "',"  \
+                   "'" + str(Q_data4) + "',"  \
+                   + str(Q_data5) + ","  \
+                   + str(Q_data6) + ","  \
+                   + str(Q_data7) + ");"
+    # print(insert_query)
+    Database.excute_Query(cursor, insert_query)
+    DBconn.commit()
+
+
 #Excel 데이터 확인 쿼리
+Database.excute_Query(cursor, "SELECT * FROM SampleData")
+for a in cursor.fetchall():
+    print(a)
 Database.disconnect_DB(DBconn) #데이터베이스 종료
 
 
